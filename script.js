92d88e1be10cbe803d645c9c40f993019dfa0e7f
Colin Park <colinpark89@gmail.com>
2020-09-07 19:31:32 -0500

    Done for the night

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
 // Write your JavaScript code here!
 
+function checkDefaults(e) {
+   let pilot = document.getElementById("pilotName")
+   let coPilot = document.getElementById("copilotName")
+   let fuelLevel = document.getElementById("fuelLevel")
+   let cargoMass = document.getElementById("cargoMass")
+   if (pilot.innerHTML === '') {
+      let pilotResult = window.confirm("Please enter a pilot name");
+      // e.preventDefault()
+   }  if (coPilot.innerHTML === '') {
+      let coPilotResult = window.confirm("Please enter a co-pilot name");
+      // e.preventDefault()
+   }  if (fuelLevel.innerHTML !== Number) {
+      let fuelLevelResult = window.confirm("Please enter a fuel level");
+      // e.preventDefault()
+   }  if (cargoMass.innerHTML !== Number) {
+      let cargoMassRresult = window.confirm("Please enter a cargo mass");
+      // e.preventDefault()
+   }
+}
+window.addEventListener("load", function(){
+   let form = document.getElementById('formSubmit');
+   form.addEventListener("click", checkDefaults);
+ });
+
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
 <h2>Mission Destination</h2>
 <ol>
